91ae06482703b574ee717eadd69ecff97985e359
DiegxR <juandiegorojasarredono@gmail.com>
2023-07-14 20:02:40 -0500

    Fixed errors

--- src/core/modules/products/dto/product.dto.ts
+++ src/core/modules/products/dto/product.dto.ts
@@ -1,13 +1,6 @@
 export class ProductDto {
-<<<<<<< HEAD
-    name: string;
-    price: number;
-    date: Date;
-}
-=======
   name: string;
   price: number;
   fechaEntrega: Date;
   ingredientsIds: number[];
 }
->>>>>>> 3d34fccfd7f2a3f719a2fe615846a7ae19acf8c6
--- src/core/modules/products/entity/product.entity.ts
+++ src/core/modules/products/entity/product.entity.ts
@@ -17,15 +17,9 @@ export class Products {
   @Column()
   price: number;
 
-<<<<<<< HEAD
-  @Column({type: 'date'})
-  date: Date;
-  
-=======
   @Column({ type: 'date' })
   fechaEntrega: Date;
 
->>>>>>> 3d34fccfd7f2a3f719a2fe615846a7ae19acf8c6
   @ManyToMany(() => Ingredients, (ingredients) => ingredients.product)
   @JoinTable()
   ingredients: Ingredients[];
